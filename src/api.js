d144c2512b12e44b8cac8c02359e79aa653031f9
Ezinwa Okpoechi <brainmaestro@outlook.com>
2017-03-20 04:40:58 +0100

    Add external API call and display

---
+++ src/api.js
@@ -0,0 +1,8 @@
+const BASE_URL = 'https://www.metaweather.com/api/location';
+
+export const getWeather = city => {
+  return fetch(`${BASE_URL}/search/?query=${city}`)
+    .then(response => response.json())
+    .then(results => fetch(`${BASE_URL}/${results[0].woeid}`))
+    .then(response => response.json())
+}
--- src/preview.js
+++ src/preview.js
@@ -1,13 +1,38 @@
 import React, { Component, PropTypes } from 'react'
+import { getWeather } from './api';
+
+const headers = {
+  weather_state_name: 'State',
+  applicable_date: 'Date',
+  min_temp: 'Min Temp',
+  max_temp: 'Max Temp',
+  the_temp: 'Temp',
+  humidity: 'Humidity',
+}
 
 export default class Preview extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      weather: null,
+    }
+  }
+
+  componentDidMount() {
+    getWeather(this.props.city).then(weather => this.setState({ weather }))
+  }
+
   render() {
-    const { city } = this.props
+    if (! this.state.weather) return <div>Loading...</div>
+
+    const weather = this.state.weather.consolidated_weather[0]
 
     return (
-      <div>
-        {city == 'london' ? 'Endless Rain' : 'Look Outside'}
-      </div>
+      <ul>
+        {Object.keys(headers).map(metric => (
+          <ul key={metric}>{`${headers[metric]}: ${weather[metric]}`}</ul>
+        ))}
+      </ul>
     )
   }
 }
