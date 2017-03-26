4d8e934c61f12a5407772a62aca07c4ff6775c6a
Ezinwa Okpoechi <brainmaestro@outlook.com>
2017-03-26 23:03:59 +0100

    Memoize api call for one hour

--- src/api.js
+++ src/api.js
@@ -1,8 +1,10 @@
+import { memoize } from 'cerebro-tools'
+
 const BASE_URL = 'https://www.metaweather.com/api/location';
 
-export const getWeather = city => {
+export const getWeather = memoize(city => {
   return fetch(`${BASE_URL}/search/?query=${city}`)
     .then(response => response.json())
     .then(results => fetch(`${BASE_URL}/${results[0].woeid}`))
     .then(response => response.json())
-}
+}, { maxAge: 1000 * 3600 }) // memoize for one hour
