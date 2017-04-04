41a12ffc2df9759ac4edb2cab1d160f0731a995a
Ezinwa Okpoechi <brainmaestro@outlook.com>
2017-04-04 12:27:11 +0100

    Debounce API calls

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@
 
 import React from 'react'
 import Preview from './preview'
+import debounce from 'lodash/debounce'
 
-export const fn = ({ term, display }) => {
+export const fn = debounce(({ term, display }) => {
   display({
     title: `Weather in ${term}`,
     subtitle: 'Cloudy with a chance of meatballs',
     getPreview: () => <Preview city={term} />
   })
-}
+}, 250)
