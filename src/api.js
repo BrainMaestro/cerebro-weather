d8b4c4e453fbda1c753b48924fbbcc100fe3bf64
Ezinwa Okpoechi <brainmaestro@outlook.com>
2017-04-04 12:27:11 +0100

    Memoize api functions

--- src/api.js
+++ src/api.js
@@ -1,10 +1,25 @@
 import { memoize } from 'cerebro-tools'
+import { head, prop, curry } from 'ramda'
 
-const BASE_URL = 'https://www.metaweather.com/api/location';
+const BASE_URL = 'https://www.metaweather.com/api/location'
 
-export const getWeather = memoize(city => {
-  return fetch(`${BASE_URL}/search/?query=${city}`)
-    .then(response => response.json())
-    .then(results => fetch(`${BASE_URL}/${results[0].woeid}`))
-    .then(response => response.json())
-}, { maxAge: 1000 * 3600 }) // memoize for one hour
+const lifetime = { maxAge: 1000 * 3600 }
+
+const searchCity = memoize(
+  city => fetch(`${BASE_URL}/search/?query=${city}`).then(decode),
+  lifetime
+)
+
+const getCityWeather = memoize(
+  woeid => fetch(`${BASE_URL}/${woeid}`).then(decode),
+  lifetime
+)
+
+const decode = response => response.json()
+
+export const getWeather = city => (
+  searchCity(city)
+    .then(head)
+    .then(prop('woeid'))
+    .then(getCityWeather)
+)
